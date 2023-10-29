18c5a4d2fe71508949341fa80ddb00ac99e62a8c
Hitesh <hiteshparmar782002@gmail.com>
2023-10-29 15:38:43 +0530

    Initial commit

---
+++ src/test/java/com/example/springGithub/SpringGithubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springGithub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGithubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
